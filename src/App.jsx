50bcc5f9a95b75a750369932e58b0908d5dfed89
Matheus Merlo <matheus.merlo02@gmail.com>
2025-03-17 22:22:21 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./components/Button"
+
+export default function App() {
+  return(
+  <div className="App">
+    <h1>Olá mundo</h1>
+    <Button title="Entrar"/>
+    <Button title="Fechar"/>
+    <Button title="Abrir"/>
+    <Button title="Salvar"/>
+    <Button title="Atualizar"/>
+  </div>
+  );
+}
